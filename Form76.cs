01924030c43708f3e172aa50e613bdd5d70b5313
AntonioDusti1 <152974953+AntonioDusti1@users.noreply.github.com>
2024-05-21 12:16:00 +0200

    Add files via upload

---
+++ Form76.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadatak21
+{
+    public partial class Form1 : Form
+    {
+        List<Prosjek> listaOcjena = new List<Prosjek>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //todo napraviti provjeru podataka 
+            //try...catch
+            //ocjena mora biti od 1 do 5
+            // predmet ne smije biti prazan string
+            Prosjek prosjek = new Prosjek(comboBox1.Text, Convert.ToInt32(textBox1.Text));
+            listaOcjena.Add(prosjek);
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            foreach(Prosjek prosjek in listaOcjena)
+            {
+                textBox2.AppendText(prosjek.NazivPredmeta
+                    + "\t" +
+                    prosjek.Ocijene + "\r\n");
+            }
+        }
+
+        private void Učitaj_Click(object sender, EventArgs e)
+        {
+            //Ovdje ide kod za učitavanje .csv datoteke
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Prosjek.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak21
+{
+    internal class Prosjek
+    {
+        //todo
+        string nazivPredmeta;
+        int ocijene;
+
+        public Prosjek(string nazivPredmeta, int ocijene)
+        {
+            this.NazivPredmeta = nazivPredmeta;
+            this.Ocijene = ocijene;
+        }
+        public Prosjek()
+        {
+
+         }
+        public string NazivPredmeta { get => nazivPredmeta; set => nazivPredmeta = value; }
+        public int Ocijene { get => ocijene; set => ocijene = value; }
+
+    }
+}
